0003822ceea23f12fec891b6d50c5ed4e8ae946c
Carl Gray <carl@firstfnf.com>
2019-02-20 22:53:27 +1100

    some progress

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,31 @@
 <template>
 <div>
- Hi there!   
+ <SearchBar @termChange="onTermChange" ref="my-child" ></SearchBar>  
 </div>
 </template>
 
 <script>
+import axios from 'axios';
+
+import SearchBar from './components/SearchBar';
+const API_KEY = process.env.VUE_APP_API_KEY;
+
 export default {
-    name: 'App'
+    name: 'App',
+    components: {
+        SearchBar
+    },
+    methods: {
+        onTermChange(searchTerm) {
+            axios.get('https://www.googleapis.com/youtube/v3/search', {
+                params: {
+                    key: API_KEY,
+                    type: 'video',
+                    part: 'snippet',
+                    q: searchTerm
+                }
+            }).then(response => console.log(response));
+        }
+    }
 };
 </script>
